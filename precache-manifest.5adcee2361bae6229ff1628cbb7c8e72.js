bfbf624cd2499e8485626076c336adc7fa3b23d4
Andre Herculano <andresilveirah@gmail.com>
2019-12-01 13:33:54 +0100

    Updates

---
+++ precache-manifest.5adcee2361bae6229ff1628cbb7c8e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeba69cec2b889abc378d93a5b0b1f7",
+    "url": "/iab-consent-string-explorer/index.html"
+  },
+  {
+    "revision": "021233f5d78dcb9dda9e",
+    "url": "/iab-consent-string-explorer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "94ec69ade088a75e24be",
+    "url": "/iab-consent-string-explorer/static/js/2.e6e630e6.chunk.js"
+  },
+  {
+    "revision": "021233f5d78dcb9dda9e",
+    "url": "/iab-consent-string-explorer/static/js/main.c7f4dc82.chunk.js"
+  },
+  {
+    "revision": "6f54adf6d034f2a0435d",
+    "url": "/iab-consent-string-explorer/static/js/runtime-main.4019a29a.js"
+  }
+]);
